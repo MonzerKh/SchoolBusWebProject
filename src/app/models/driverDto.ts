058a8faff85d1dff09e1aafe0096be69dde8a126
Aufa alkarfan <awfa.alkrfan@gmail.com>
2022-01-06 02:33:51 +0300

    driverDto

---
+++ src/app/models/driverDto.ts
@@ -0,0 +1,14 @@
+export interface DriverDto{
+  id: number;
+  full_Name:string;
+  email: string;
+  phone: string;
+  company: string;
+  personalImage: string;
+  imagePath:string;
+  busCompany_Id: number;
+  userName: string;
+  systemUser_Id: number;
+  createdBy: number;
+  updateBy: number;
+}
---
+++ src/app/models/driverParams.ts
@@ -0,0 +1,34 @@
+import { PaginationSource } from 'src/app/models/pagination';
+
+
+export class DriverParams {
+  full_Name! :string;
+  address! :string;
+  email! :string;
+  phone! :string;
+
+
+
+  Pagination!:PaginationSource;
+  constructor() {
+    this.Pagination = new PaginationSource();
+  }
+
+  getHttpParams() {
+    let Params = this.Pagination.getPaginationHeaders();
+
+    if (this.full_Name) { Params = Params.append('full_Name', this.full_Name.toString()); }
+    if (this.address) { Params = Params.append('address', this.address.toString()); }
+    if (this.email) { Params = Params.append('email', this.email.toString()); }
+    if (this.phone) { Params = Params.append('phone', this.phone.toString()); }
+
+    return Params;
+  }
+
+  setPagination(page:PaginationSource){
+    this.Pagination.currentPage = page.currentPage;
+    this.Pagination.totalItems = page.totalItems;
+    this.Pagination.totalPages = page.totalPages;
+    this.Pagination.itemsPerPage = page.itemsPerPage;
+  }
+}
