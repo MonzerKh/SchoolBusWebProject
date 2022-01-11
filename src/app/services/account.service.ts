d9476ef603208300085a33dcd0389c5e132aace7
Monzer <Monzer.al.khiami@itm-techsoft.com>
2022-01-11 15:46:13 +0300

    fix image problem

---
+++ src/app/services/account.service.ts
@@ -0,0 +1,58 @@
+import { environment } from './../../environments/environment.prod';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { LoginDto } from '../models/loginDto';
+
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { SystemUserDto } from '../models/systemUserDto ';
+import { Router } from '@angular/router'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new ReplaySubject<SystemUserDto>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
+
+  login(loginDto: LoginDto) {
+
+    return this.http.post(this.baseUrl + 'Account/login', loginDto)
+      .subscribe((res: SystemUserDto | any) => {
+        let user: SystemUserDto;
+        user = res;
+        console.log(user);
+        if (user) {
+          this.setCurrentUser(user);
+        //  this.router.navigateByUrl('/home')
+        }
+      })
+  }
+
+  setCurrentUser(user: SystemUserDto) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUserSource.next(user);
+  }
+
+  getCurrentUser() :boolean {
+    const user: SystemUserDto = JSON.parse(localStorage.getItem('user')!);
+    if (user) {
+      this.setCurrentUser(user);
+      return true;
+    }
+    return false;
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null!);
+    //this.currentUserSource = new ReplaySubject<SystemUserDto>(1);
+    this.router.navigateByUrl('/sign-in');
+  }
+}
