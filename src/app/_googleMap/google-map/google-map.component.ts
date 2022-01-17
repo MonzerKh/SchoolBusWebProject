54876c1c7ffaeb8df1cd1b2a12049f649f7f9f77
Aufa alkarfan <awfa.alkrfan@gmail.com>
2022-01-17 10:57:04 +0300

    last

--- src/app/_googleMap/google-map/google-map.component.ts
+++ src/app/_googleMap/google-map/google-map.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+<<<<<<< HEAD
+import { Component, OnInit } from '@angular/core';
 
+=======
+import { Component, OnInit } from '@angular/core';
+>>>>>>> 592f8c8bb6ecdad3ba401fa07f75ac89256dc6c3
 
 @Component({
   selector: 'app-google-map',
   templateUrl: './google-map.component.html',
-  styleUrls: ['./google-map.component.scss'],
+  styleUrls: ['./google-map.component.scss']
 })
+export class GoogleMapComponent implements OnInit {
 
+  constructor() { }
 
-export class GoogleMapComponent {
-
-
-
+  ngOnInit(): void {
   }
+
+}
+
+
 
 
